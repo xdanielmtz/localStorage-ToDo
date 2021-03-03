06fbfb06c222879c1147ea57ba2a4821e1b95145
Dani JM <xdanielmtz@gmail.com>
2021-03-02 21:37:03 -0500

    final touches

--- script.js
+++ script.js
@@ -3,37 +3,82 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+var todos = [];
 
-function renderTodos () {
-    todoCountSpan.textContent = todos.length; 
-    for(var i =0; i < todos.length; i++){
-        var liEL = document.createElement("li");
-        liEL.textContent =todos[i]
-        liEL.setAttribute("data-index", 1);
-            var newButton = document.createElement("button");
-            newButton.textContent = "Complete";
-            liEL.append(newButton);
-        todoList.append(liEL);
-    }
+init();
+
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
 }
 
-todoForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var newTodo = todoInput.value;
-    console.log(newTodo);
-    todos.push(newTodo);
+function init() {
+  // Get stored todos from localStorage
+  // Parsing the JSON string to an object
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // Render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set "todos" key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// When form is submitted...
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  todoInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+});
+
+// When a element inside of the todoList is clicked...
+todoList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // If that element is a button...
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
     renderTodos();
-})
-
-
-renderTodos();
-
-todoList.addEventListener("click", function (event) {
-    if(event.target.matches("button")){
-        var indexToComplete = event.target.parentElement.getAttribute("data-index");
-        console.log(indexToComplete)
-        todos.splice(indexToComplete, 1);
-        renderTodos();
-    }
-})
+  }
+});
